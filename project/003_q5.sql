899d0b9cd5608463d85af431c0c8da5d82a8f517
David Garfinkle <david@kn0x.io>
2019-03-02 02:57:12 -0500

    Correct q5

--- project/003_q5.sql
+++ project/003_q5.sql
@@ -8,7 +8,7 @@ SELECT onset, "pitch-b40" FROM Note JOIN Piece ON piece_id=Piece.id LIMIT 20;
 SELECT piece_id, onset, array_agg("pitch-b40") as chords FROM Note GROUP BY piece_id, onset ORDER BY piece_id, onset LIMIT 20;
 
 -- [subquery] measures without any notes... i.e., whole rests
-SELECT num FROM Measure WHERE NOT EXISTS ( SELECT measure FROM Note ) LIMIT 20;
+SELECT measure.piece, num FROM Measure WHERE num NOT IN (SELECT measure FROM Note) LIMIT 20;
 
 -- [select] pieces by composer X
 SELECT name FROM Piece WHERE composer='Josquin Des Prez' LIMIT 20;
