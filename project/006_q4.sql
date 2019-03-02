5fb90cfcf01c22bca31e649e11b25f309cdcb8b7
David Garfinkle <david@kn0x.io>
2019-03-02 02:40:11 -0500

    remove data from q4; include piece

--- project/006_q4.sql
+++ project/006_q4.sql
@@ -1,6 +1,7 @@
 SELECT * FROM Composer LIMIT 5;
 SELECT * FROM Corpus LIMIT 5;
-SELECT * FROM Measure LIMIT 5;
+SELECT id, num, piece FROM Measure LIMIT 5;
+SELECT id, encoding, composer, corpus, name FROM Piece LIMIT 5;
 SELECT * FROM Part LIMIT 5;
 SELECT * FROM Note LIMIT 5;
 SELECT * FROM Trigram LIMIT 5;
